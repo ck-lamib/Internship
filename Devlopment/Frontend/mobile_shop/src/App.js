8931adaf9c90627987a96a0c6d7135976e19e2d0
atish grung <atishgurung13@gmail.com>
2023-01-23 16:19:59 +0545

    Adding slider component

--- Devlopment/Frontend/mobile_shop/src/App.js
+++ Devlopment/Frontend/mobile_shop/src/App.js
@@ -1,10 +1,19 @@
+// import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './NavBar';
+import { Product } from './Product';
+import { Slider } from './Slider.js';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <NavBar />
+      <Slider />
+      <Product/>
+      {/* product component
+      footer */}
     </div>
+
   );
 }
 
---
+++ Devlopment/Frontend/mobile_shop/src/Slider.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import './App.css';
+
+export const Slider = () => {
+  return (
+    <div>
+        <section id="slider">
+        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img className="slide-img" src="https://www.themobileshop.ca/static/a157e0eb3386d4b4e24745c7ed11af98/d04c2/kiosk-medium_2021.jpg"  alt="..."/>
+                <div className="carousel-caption d-none d-md-block">
+                  <h5>First slide label</h5>
+                  <p>Some representative placeholder content for the first slide.</p>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <img className="slide-img" src="https://lh3.googleusercontent.com/p/AF1QipOmQoi5cx2YZY-gh0BUylww7qBo1jPjeGDURX9r=s1600-w1280?downsize=330:*" alt="..."/>
+                <div className="carousel-caption d-none d-md-block">
+                  <h5>Second slide label</h5>
+                  <p>Some representative placeholder content for the second slide.</p>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <img className="slide-img" src="https://s3b.cashify.in/gpro/uploads/2022/06/10123008/Best-Samsung-second-hand-mobile-phone-to-buy-in-India.jpg" alt="..."/>
+                <div className="carousel-caption d-none d-md-block">
+                  <h5>Third slide label</h5>
+                  <p>Some representative placeholder content for the third slide.</p>
+                </div>
+              </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+    </section>
+    </div>
+  )
+}
